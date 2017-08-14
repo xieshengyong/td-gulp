dd86f5163bcce7b99e83b817ee1e79fbb0b5a4ab
formaction <formaction@163.com>
2017-08-14 19:10:28 +0800

    优化预加载模块； 1、简化API，去掉on事件 2、添加onLoad、setPath方法 3、常规优化

--- src/js/app/module/Preload.js
+++ src/js/app/module/Preload.js
@@ -2,27 +2,33 @@
     常规H5图片及逐帧数据加载器
 
     特点：
-        1、支持加载字符串形式单张图片、数组形式多张图片、逐帧数据
+        1、支持加载字符串形式单张图片、数组形式多张图片、逐帧数据、键值对(hash)对象
         2、链式调用
         3、自定义缓存中图片别名
-        4、缓存器 Preload.Buffer
+        4、寄存器 Preload.Buffer
+
+    API: add(string|Array|Object)   添加加载对象
+        load(function)  加载过程，回调参数p为加载百分百
+        onLoad(function)    加载完成
+        setPath(string)     重设加载路径
 
     使用：
         var loader = new Preload.Loader('imgPath/');
         loader.add('aa.png')
             .add(['a2.png', 'a3.png'])
-            .add({'name', 'a4.png'})
-            .on('progress', function (p) {
+            .add({name1: 'name1.png', name2: 'name2.png'})
+            .onLoad(function (p) {
                 console.log(p);
             })
             .load(function () {
                 console.log('succeed');
             });
+    注意：支持加载的json格式为texturepacker(http://www.codeandweb.com/texturepacker)生成的普通json(Array)数据；
 */
 
 var Preload = {};
 
-// 存储器
+// 寄存器
 Preload.Buffer = {};
 
 // 加载器
@@ -47,25 +53,32 @@ fn.add = function (para) {
         return this;
     };
     if (para instanceof Object) {
-        var name = Object.getOwnPropertyNames(para)[0];
-        this.loadImg(para[name], name);
-        this.loadNum++;
+        var keys = Object.getOwnPropertyNames(para);
+        for (var i = keys.length - 1; i >= 0; i--) {
+            var key = keys[i];
+            this.loadImg(para[key], key);
+            this.loadNum++;
+        }
         return this;
     };
 };
 
-// 判断分流
+// 再次判断分流
 fn.loadType = function (para) {
     if (para instanceof Object) {
         var name = Object.getOwnPropertyNames(para)[0];
         this.loadImg(para[name], name);
         return;
     }
-    if (para.indexOf('.json') === -1) {
-        this.loadImg(para);
-    } else {
+    if (para.indexOf('.json') > -1) {
         this.loadAjax(para);
+        return;
     }
+    if (/.png|.jpeg|.png|.svg|.gif/.test(para)) {
+        this.loadImg(para);
+        return;
+    }
+    console.error('无法加载的资源类型！', para);
 };
 
 // 加载单个图片
@@ -88,7 +101,8 @@ fn.loadImg = function (src, name) {
 
 // 加载图片数组
 fn.loadImgArray = function (arr) {
-    for (var i = 0; i < arr.length; i++) {
+    var aLength = arr.length;
+    for (var i = 0; i < aLength; i++) {
         this.loadType(arr[i]);
     }
 };
@@ -113,16 +127,20 @@ fn.readAjax = function (data) {
         data = JSON.parse(data);
     };
     var img = new Image();
+    var frames = data.frames;
+    var fLength = frames.length;
     img.onload = function () {
-        for (var name in data.frames) {
+        for (var i = 0; i < fLength; i++) {
+            var tframe = frames[i];
+            var tframeF = tframe.frame;
             var imgData = {};
             imgData.image = img;
-            imgData.x = data.frames[name].frame.x;
-            imgData.y = data.frames[name].frame.y;
-            imgData.w = data.frames[name].frame.w;
-            imgData.h = data.frames[name].frame.h;
-            Preload.Buffer[data.frames[name].filename] = imgData;
-        };
+            imgData.x = tframeF.x;
+            imgData.y = tframeF.y;
+            imgData.w = tframeF.w;
+            imgData.h = tframeF.h;
+            Preload.Buffer[tframe.filename] = imgData;
+        }
         self.updateProgress();
     };
     img.src = this.path + data.meta.image;
@@ -139,16 +157,21 @@ fn.updateProgress = function () {
 };
 
 // 绑定事件
-fn.on = function (event, fn) {
-    if (event === 'progress') {
-        this.updateCallBack = fn;
-    }
+fn.onLoad = function (fn) {
+    this.updateCallBack = fn;
     return this;
 };
 
 // 加载完成
 fn.load = function (fn) {
-    this.loadCallBack = fn;
+    this.loadCallBack = fn.bind(Preload.Buffer);
+    return this;
+};
+
+// 重设加载路径
+fn.setPath = function (path) {
+    this.path = path;
+    return this;
 };
 
 module.exports = Preload;
